a31ecbbf837801dff01894ea784e3067aaefe698
Raqui333 <klebersilva2025@gmail.com>
2024-11-16 15:40:58 -0300

    3301

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# frontend_test
-Front-End
+# Instabytes
+
+Este projeto é um site que simula um perfil no estilo do Instagram. Ele foi desenvolvido para praticar habilidades em **HTML**, **CSS** e **JavaScript**, e utiliza APIs para gerar informações dinâmicas e conteúdo visual.  
+
+## Tecnologias Utilizadas  
+- **HTML5, CSS3 e JavaScript**: Para estrutura, estilização e interatividade do site.  
+- **GrapesJS**: Ferramenta de edição visual utilizada para criar a estrutura inicial do HTML e do CSS.  
+- **APIs**:  
+  - [Random User API](https://randomuser.me/api): Para gerar informações fictícias de usuários.  
+  - [Picsum Photos](https://picsum.photos/1920/1080): Para carregar imagens aleatórias.  
+
+## Funcionalidades  
+- Exibição de um perfil fictício com foto, nome de usuário e estatísticas aleatórias.  
+- Feed com um número entre 3 e 100 de imagens geradas aleatoriamente.
+- Visualizador de imagens em tela cheia com informações adicionais.  
+- Geração dinâmica de conteúdo ao carregar a página.  
+
+## Objetivo do Projeto  
+Este projeto foi desenvolvido para:  
+1. Aprender e aplicar conceitos básicos de frontend.  
+2. Praticar o consumo de APIs públicas para integração de dados dinâmicos.  
+3. Explorar ferramentas de criação visual como o GrapesJS.  
+4. Criar uma interface interativa e responsiva, semelhante a uma aplicação real.
---
+++ src/main.js
@@ -0,0 +1,89 @@
+function genRandomPost() {
+  fetch('https://picsum.photos/1920/1080', {
+    method: 'GET',
+  }).then((res) => {
+    const feed = document.getElementById('ihkk6');
+
+    if (feed.querySelector('h1')) feed.querySelector('h1').remove();
+
+    let img = document.createElement('img');
+    img.src = res.url;
+    img.className = 'post_img';
+
+    img.onclick = () => openImgViewer(res.url);
+
+    feed.appendChild(img);
+  });
+}
+
+function genRandomProfile() {
+  const url = 'https://randomuser.me/api';
+  fetch(url, {
+    method: 'GET',
+  }).then(async (res) => {
+    const data = await res.json();
+    const { picture, login } = data.results[0];
+    document.getElementById('id2zt').src = picture.large;
+    document.getElementById('ikqd5').innerText = '@' + login.username;
+  });
+}
+
+function getRandomNumberBetween(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function setupPage() {
+  genRandomProfile();
+
+  const posts = getRandomNumberBetween(3, 100);
+  for (let i = 0; i <= posts; i++)
+    genRandomPost(); // prettier-ignore
+
+  document.getElementById('iynq7').innerText = posts + ' Publicações';
+
+  document.getElementById('i6z6d').innerText =
+    getRandomNumberBetween(0, 999) + ' Seguidores';
+
+  document.getElementById('iws8o').innerText =
+    getRandomNumberBetween(0, 999) + ' Seguindo';
+}
+
+function openImgViewer(url) {
+  const image_viewer = document.createElement('div');
+  image_viewer.id = 'i2gh';
+
+  image_viewer.innerHTML = `
+      <div id="ia2j" onclick="closeImgViwer()">
+        <img id="iwvbe" onclick="closeImgViwer()" src="./res/closer_icon.png"/>
+        <img id="i29r3" src="${url}"/>
+        <div id="ifdpq">
+          <div id="ifnot">
+            <div id="iwt1s">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus 
+              mollis ante sed dolor euismod semper. Proin pulvinar venenatis urna, 
+              et facilisis eros lacinia et. Sed ornare tortor mauris, efficitur 
+              facilisis metus laoreet non.
+              <br/>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+
+  document.body.appendChild(image_viewer);
+  document.body.classList.add('disable-scrolling');
+}
+
+function closeImgViwer() {
+  const viewer = document.getElementById('i2gh');
+
+  if (viewer) {
+    viewer.remove();
+
+    document.querySelectorAll('.post_img').forEach((elem) => {
+      elem.onclick = () => openImgViewer(elem.src);
+    });
+
+    document.body.classList.remove('disable-scrolling');
+  }
+}
